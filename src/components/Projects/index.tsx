8ed3bf98a76c5034d507d7eb4d7447432b5d87b6
Welton Nogueira <welton.nogueira.dev@gmail.com>
2023-02-19 22:54:42 -0300

    .

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,4 +1,6 @@
 import styles from '@/styles/Projects.module.scss';
+import Link from 'next/link';
+import { BookOpen, InstagramLogo, LinkedinLogo, User } from 'phosphor-react';
 import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Project } from './Project';
@@ -9,7 +11,7 @@ export const Projects = () => {
   useEffect(() => {
     if (window.innerWidth >= 1024) {
       setChunkLimitter(4);
-    } else if (window.innerWidth >= 600) {
+    } else {
       setChunkLimitter(1);
     }
   });
@@ -31,12 +33,29 @@ export const Projects = () => {
           <h2>Meus Projetos</h2>
         </header>
         <div>
-          <button className={styles.active}>Pessoais</button>
-          <button>Acadêmicos</button>
+          <button className={styles.active}>
+            <User
+              size={20}
+              weight="bold"
+              color="#222222"
+            />
+            Pessoais
+          </button>
+          <button>
+            <BookOpen
+              size={20}
+              weight="bold"
+            />
+            Acadêmicos
+          </button>
         </div>
       </div>
       <div className={styles.projectsContainer}>
-        <Swiper>
+        <Swiper
+          spaceBetween={10}
+          style={{
+            height: 'calc(100% - 61px - 2rem);',
+          }}>
           {chunks.map((chunk: any) => {
             return (
               <SwiperSlide>
@@ -49,6 +68,21 @@ export const Projects = () => {
             );
           })}
         </Swiper>
+        <small>Arraste para ver mais.</small>
+        <div className={styles.social}>
+          <Link href="#">
+            <LinkedinLogo
+              color="#ffffff"
+              size={24}
+            />
+          </Link>
+          <Link href="#">
+            <InstagramLogo
+              color="#ffffff"
+              size={24}
+            />
+          </Link>
+        </div>
       </div>
     </section>
   );
